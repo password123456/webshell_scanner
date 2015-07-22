c638fa06afc7aa32dfccf66cae87b93c807f2240
password123456 <sendmefuture@gmail.com>
2015-07-22 16:14:44 +0900

    Update quick_scan_webshell.py

--- quick_scan_webshell.py
+++ quick_scan_webshell.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """
  WebShell Scanner Based on patterns.
  created by password123456.
 """
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os,sys,time
 import codecs
 import argparse
@@ -56,12 +56,11 @@ def get_filehash(filename, hash):
         print "Error:" + err.strerror
         sys.exit(1)
 
-
-
 def do_scan_chinesse(filename):
     f = codecs.open(filename, 'r', 'utf-8')
     global chinese_words_counter
-    chinese_words_counter = 1
+    chinese_words_counter = 0
+    chinese_detect = 0
 
     for n,line in enumerate(f.read().split('\n')):
         n += 1
@@ -72,29 +71,34 @@ def do_scan_chinesse(filename):
     f.close()
 
     if chinese_detect >= 1:
-        print ' => Scan Chinese words : [count /', chinese_words_counter,']'
+        print ' [*] Scan Chinese Words : [ count /',chinese_words_counter,']'
     else:
-        print ' => Scan Chinese words : clean '
+        print ' [*] Scan Chinese Words : [ count /',chinese_words_counter,']'
 
 def do_scan_shell_sigs(filename):
     global php_detect_counter
     global asp_detect_counter
     global jsp_detect_counter
 
-    PHP_SIGS = r"(?si)(preg_replace.*\/e|`.*?\$.*?`|\bpassthru\b|\bshell_exec\b|\bexec\b|\bbase64_decode\b|\beval\b|\bsystem\b|\bproc_open\b|\bpopen\b|\bcurl_exec\b|\bcurl_multi_exec\b|\bparse_ini_file\b|\bshow_source\b|687474703a2f2f377368656c6c2e676f6f676c65636f64652e636f6d2f73766e2f6d616b652e6a7067|uniqid|ec38fe2a8497e0a8d6d349b3533038cb|88f078ec861a3e4baeb858e1b4308ef0|TBNnGMfflrqBFnaes|\\x50\\x4b\\x05\\x06\\x00\\x00\\x00\\x00|9c3a9720372fdfac053882f578e65846|shell)"
+    PHP_SIGS = r"(?si)(\bpassthru\b|\bshell_exec\b|\bexec\b|\bbase64_decode\b|\beval\b|\bsystem|\bproc_open\b|\bpopen\b|\bcurl_exec\b|\bcurl_multi_exec\b|\bparse_ini_file\b|\bshow_source\b|\b687474703a2f2f377368656c6c2e676f6f676c65636f64652e636f6d2f73766e2f6d616b652e6a7067\b|\buniqid\b|\bec38fe2a8497e0a8d6d349b3533038cb\b|\b88f078ec861a3e4baeb858e1b4308ef0\b|\bTBNnGMfflrqBFnaes\b|\bx50\\x4b\\x05\\x06\\x00\\x00\\x00\\x00\b|\bx50\\x4b\\x03\\x04\\x0a\b|\b9c3a9720372fdfac053882f578e65846\b|\bshell\b|\bXSLTProcessor\b|\bWebShell\b|\berror_reporting\b|\beditfile\b|\backdoor\b|\betc\/\passwd\b|\betc\/\shadow\b|\bmysql_query\b|\bphpinfo\b|\bcgi-script\b|\bgethostbyname\b|\bphp.ini\b|\bmysql.user\b|\bbash_history\b|\bSHOW\s*FIELDS\s*FROM\b|\bmysqladmin\b|\bSERVER_ADMIN\b|\bDOCUMENT_ROOT\b|\bGRANT\s*ALL\s*PRIVILEGES\b|\bgetcwd\b|\bHTTP_HOST\b|\bdisplay_errors\b|\ballow_url_fopen\b|\bbase_convert\b|\b63a9f0ea7bb98050796b649e85481845\b|\b7b24afc8bc80e548d66c4e7ff72171c5\b|\bgcc\s*-o\b)"
 
-    ASP_SIGS = r"(?si)(\bsystem32\b|\bcmd.exe\b|FromBase64String|SCRIPT\s*RUNAT=SERVER\s*LANGUAGE=JAVASCRIPT|PublicKeyToken\\=B03F5F7F11D50A3A|\bServer.ScriptTimeout\b|\bclsid:F935DC22-1CF0-11D0-ADB9-00C04FD58A0B\b|clsid:72C24DD5-D70A-438B-8A42-98424B88AFB8\b|20132165414621325641311254123112512|mssql导库|\bmssql database export\b|1c1f81a8b0a630f530f52fa9aa9dda1b|gif89|WebSniff(.*?)Powered\s*by|法客论坛|F4ckTeam|(?:ExecuteStatement)\(.*?request|#@~\^bGsBAA==@#@&AC13`DV{J@!8D@\*@!8D@\*@!\^n|\bdownload file\b|silicname|silicpass|命令行执行|server.mappath|VBScript.Encode|kernel32.dll|DirectoryInfo|F4ck|HKEY_LOCAL_MACHINE|HKEY_CURRENT_USER)"
+    ASP_SIGS = r"(?si)(\bsystem32\b|\bcmd.exe\b|\bFromBase64String\b|\bSCRIPT\s*RUNAT=SERVER\b|\bs*LANGUAGE=JAVASCRIPT\b|\bPublicKeyToken=B03F5F7F11D50A3A\b|\bServer.ScriptTimeout\b|\bclsid:F935DC22-1CF0-11D0-ADB9-00C04FD58A0B\b|\bclsid:72C24DD5-D70A-438B-8A42-98424B88AFB8\b|20132165414621325641311254123112512\b|\bmssql导库\b|\bmssql\s*database\s*export\b|\b1c1f81a8b0a630f530f52fa9aa9dda1b\b|\bgif89a\b|\bWebSniff(.*?)Powered\s*by\b|\bF4ckTeam\b|(?:ExecuteStatement)\(.*?request|#@~\^bGsBAA==@#@&AC13`DV{J@!8D@\*@!8D@\*@!\^n|\bdownload\s*file\b|\bsilicname\b|\bsilicpass\b|\bserver.mappath\b|\bVBScript.Encode\b|\bkernel32.dll\b|\bDirectoryInfo\b|\bF4ck\b|\bHKEY_LOCAL_MACHINE\\SYSTEM\b|\bServices\\Tcpip\\EnableSecurityFilters\b|\b\\hzhost\\config\\settings\\mastersvrpass\b|\b\\Microsoft\\SchedulingAgent\\LogPath\b|\bwsh.regRead\b|\bwsh.regWrite\b|\bwsh.regDelete\b|\b\\Terminal\s*Server\\Wds\\rdpwd\\Tds\\tcp\\PortNumber\b|\bXP_REGWRITE\b|\bXP_REGREAD\b|\bXP_CMDSHELL\b|\bMICROSOFT.JET.OLEDB\b|\bHKEY_CURRENT_USER\b|\bmiemie\b|\bWWWRoot\b|\bFolderPath\b|\bGetFile\b|\bserver.scripttimeout\b|\bserver_software\b|\bCONST_FSO\b|\bRQSFileManager\b|\bWINDOWS\\\TEMP\b|\bpass=request\b|\bclientPassword\b|\bREG_DWORD\b|\bREG_BINARY\b|\bREG_EXPAND_SZ\b|\bSYSTEMROOT\b|\bWNetwork.ComputerName\b|\bWNetwork.ComputerName\b|\bScripting.FileSystemObject\b|\bCreateTextFile\b|\bbase64String\b|\bADODB.Connection\b|\b\w+Rootkit\b|\bsp_oacreate\b|\bSP_OAMethod\b|\bicesword\b)"
 
-    JSP_SIGS = r"(?si)(chopper|jshell|JspSpyPwd|Alanwalker|caicaihk|caicaihk|Alanwalker|JspSpy|6625108|1decc1ce886d1b2f9f91ecb39967832d05f8e8b8|JFolder\.jsp)"
+    JSP_SIGS = r"(?si)(\bchopper\b|\bjshell\b|\bJspSpyPwd\b|\bAlanwalker\b|\bcaicaihk\b|\bcaicaihk\b|\bJspSpy\b|\b6625108\b|\b1decc1ce886d1b2f9f91ecb39967832d05f8e8b8\b|\bJFolder\.jsp\b)"
 
     php_regex = re.compile(PHP_SIGS, flags=re.IGNORECASE)
     asp_regex = re.compile(ASP_SIGS, flags=re.IGNORECASE)
     jsp_regex = re.compile(JSP_SIGS, flags=re.IGNORECASE)
 
     f = open(filename, 'r')
-    php_detect_counter = 1
-    asp_detect_counter = 1
-    jsp_detect_counter = 1
+
+    php_detect_counter = 0
+    asp_detect_counter = 0
+    jsp_detect_counter = 0
+
+    php_detect = 0
+    asp_detect = 0
+    jsp_detect = 0
 
     for n,line in enumerate(f.read().split('\n')):
         n += 1
@@ -128,9 +132,9 @@ def do_scan_shell_sigs(filename):
             for _line in line:
                 _match_line = asp_regex.findall(_line)
 
-    f.close()
-
     detect_result = php_detect + asp_detect + jsp_detect
+
+    f.close()
 
     if detect_result >= 1 :
         get_fileinfo(filename)
@@ -139,43 +143,27 @@ def do_scan_shell_sigs(filename):
         print ' [*] ASP Suspicious function used: [count /',asp_detect_counter,']'
         print ' [*] JSP Suspicious function used: [count /',jsp_detect_counter,']'
         do_scan_chinesse(filename)
-        print (':' * 20), '>> Suspicious file'
-
-        log_file(filename)
-
 
 def scan_filetype(path):
     print "Current Path: " + path
-
     for root, dirs, files in os.walk(path):
         for filename in files:
             file = os.path.realpath(os.path.join(root,filename))
             try:
                 with open(file, 'rb') as f:
                     if b'\x00' in f.read():
-                        #print('The file is binary! ', path)
-                        continue
+                        #print('The file is binary! ', file)
+                        pass
                     else:
-                        #print('The file is not binary! ', filename)
+                        #print('The file is not binary! ', file)
                         do_scan_shell_sigs(file)
             except:
                 pass
 
-def log_file(filename):
-
-    log_date = time.strftime('%Y-%m-%d %H:%M:%S')
-    file_path = os.path.abspath(filename)
-    log_file =  time.strftime('%Y%m%d') + '_scan-log.txt'
-
-    mode = 'a' if os.path.exists(log_file) else 'w'
-    with open(log_file, mode) as f:
-        f.write('[%s] PHP=%s, ASP=%s, JSP=%s, china=%s, \t %s \n' % (log_date, php_detect_counter, asp_detect_counter, jsp_detect_counter, chinese_words_counter, file_path) )
-        f.close()
-
 def main():
   opt=argparse.ArgumentParser(description="::::: Quick WebShell-Scan + Scan Chinese Words :::::")
   opt.add_argument("scan_path", help="ex) /var/www/html/upload")
-  opt.add_argument("-p", "--path", action="store_true", dest="path", help="ex) python quick_scan_webshell.py -p /var/www/html/upload")
+  opt.add_argument("-p", "--path", action="store_true", dest="path", help="ex) python quick_scan_webshell_v0.1.py -p /var/www/html/upload")
 
   if len(sys.argv)<=2:
     opt.print_help()
@@ -190,7 +178,6 @@ def main():
   else:
       opt.print_help()
       sys.exit()
-
 
 if __name__ == '__main__':
     main()
